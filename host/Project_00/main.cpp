5e7f416b7826e67df987aae728278da9b718e464
AlessandroS94 <alessandro.sallese@student.univaq.it>
2024-05-31 15:18:47 +0200

    first commit

---
+++ host/Project_00/main.cpp
@@ -0,0 +1,14 @@
+//
+// Created by Alessandro Sallese on 31/05/24.
+//
+
+#include "main.h"
+#include "stdio.h"
+
+
+int main(){
+    int a = 90;
+    printf("CIAO \n");
+    printf("Valore a : %d\n", a);
+    return 0;
+}
---
+++ host/Project_00/main.h
@@ -0,0 +1,8 @@
+//
+// Created by Alessandro Sallese on 31/05/24.
+//
+
+#ifndef PROJECT_00_MAIN_H
+#define PROJECT_00_MAIN_H
+
+#endif //PROJECT_00_MAIN_H
